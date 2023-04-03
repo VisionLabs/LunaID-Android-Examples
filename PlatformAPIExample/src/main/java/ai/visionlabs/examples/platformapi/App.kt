7589d6a83b5d62279e1d5f711e79e6458584723f
Andrey Elizarov <a.elizarov@visionlabs.ru>
2023-04-03 10:37:35 +0400

    update api example

--- PlatformAPIExample/src/main/java/ai/visionlabs/examples/platformapi/App.kt
+++ PlatformAPIExample/src/main/java/ai/visionlabs/examples/platformapi/App.kt
@@ -1,10 +1,31 @@
 package ai.visionlabs.examples.platformapi
 
 import android.app.Application
+import ru.visionlabs.sdk.lunaweb.v6.ApiHuman
+import ru.visionlabs.sdk.lunaweb.v6.ApiHumanConfig
 
 class App : Application() {
+
+    companion object {
+        @JvmStatic
+        public lateinit var platformApi: ApiHuman
+    }
+
     override fun onCreate() {
         super.onCreate()
 
+        val baseUrl = "http://luna-platform.com/api/6/"
+
+        // will be added to every request
+        val constantHeaders = mapOf(
+            "LUNA_ACCOUNT_ID_HEADER" to "12ed7399-f779-479c-8258-bbc45e6017af"
+        )
+
+        val apiConfig = ApiHumanConfig(
+            baseUrl = baseUrl,
+            headers = constantHeaders,
+        )
+
+        platformApi = ApiHuman(apiConfig)
     }
 }

736eb5045c8e55af0d09e40793767ac52614f572
Andrey Elizarov <a.elizarov@visionlabs.ru>
2023-06-22 23:37:07 +0400

    bump core. 1.3.1->1.3.2

--- PlatformAPIExample/build.gradle.kts
+++ PlatformAPIExample/build.gradle.kts
@@ -64,7 +64,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
 
 
-    implementation("ai.visionlabs.lunaid:core:1.3.1@aar")
+    implementation("ai.visionlabs.lunaid:core:1.3.2@aar")
 //    implementation(project(":lunaCore"))
 
 }

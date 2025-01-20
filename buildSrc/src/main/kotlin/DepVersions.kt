baad28d158d6b9f64420f3d2c33b300729f47f64
Alex <alez.grante@gmail.com>
2025-01-21 00:25:41 +0300

    ILUN-1610: fix face lost

--- buildSrc/src/main/kotlin/DepVersions.kt
+++ buildSrc/src/main/kotlin/DepVersions.kt
@@ -8,8 +8,8 @@ public object DepVersions {
     const val minSdkVersion = 21
     const val targetSdkVersion = 34
     const val buildToolsVersion = "34.0.0"
-//    const val ndkVersion = "21.0.6113669"
-    const val ndkVersion = "23.1.7779620"
+    const val ndkVersion = "21.0.6113669"
+//    const val ndkVersion = "23.1.7779620"
     const val cmakeVersion =  "3.22.1"
     const val sdkVersion = "1.13.2"
 

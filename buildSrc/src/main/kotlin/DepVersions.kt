9fa5ba8b54bff818535777f901d81e508189aa3a
Alex <alez.grante@gmail.com>
2025-01-22 20:16:43 +0300

    ILUN-1610: enable EdgeToEdge, remove color of status bar, refactor

--- buildSrc/src/main/kotlin/DepVersions.kt
+++ buildSrc/src/main/kotlin/DepVersions.kt
@@ -4,10 +4,10 @@
 //}
 
 public object DepVersions {
-    const val compileSdkVersion = 34
+    const val compileSdkVersion = 35
     const val minSdkVersion = 21
-    const val targetSdkVersion = 34
-    const val buildToolsVersion = "34.0.0"
+    const val targetSdkVersion = 35
+    const val buildToolsVersion = "35.0.0"
     const val ndkVersion = "21.0.6113669"
 //    const val ndkVersion = "23.1.7779620"
     const val cmakeVersion =  "3.22.1"
